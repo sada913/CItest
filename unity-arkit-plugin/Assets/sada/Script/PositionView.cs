c00cfdf73e9ac0d2e8b332351ade29b323817ccb
sada <sada913@gmail.com>
2018-11-29 18:09:09 +0900

    ARKit SetOrigin Sample 実装

---
+++ unity-arkit-plugin/Assets/sada/Script/PositionView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionView : MonoBehaviour {
+    [SerializeField] Transform arcam;
+    [SerializeField] Text text;
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.text = "(" + arcam.transform.position.x + "," + arcam.transform.position.y + "," + arcam.transform.position.z + ")";
+    }
+}
---
+++ unity-arkit-plugin/Assets/sada/Script/SetOrigin.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.iOS;
+
+public class SetOrigin : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        UnityARSessionNativeInterface.GetARSessionNativeInterface().SetWorldOrigin(transform);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
