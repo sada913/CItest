8771668561a83682e20d87d3a4fdf62a3f26f1f2
sada <sada913@gmail.com>
2018-12-05 18:40:57 +0900

    Photon での空間シェアリング

---
+++ unity-arkit-plugin/Assets/sada/PhotonShare/Script/CameraTransformSync.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransformSync : MonoBehaviour {
+    Transform m_camera;
+    PhotonView m_photonView;
+	// Use this for initialization
+	void Start () {
+        m_camera = GameObject.FindWithTag("MainCamera").transform;
+        m_photonView = GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void FixedUpdate()
+    {
+        if (m_photonView.isMine)
+            gameObject.transform.SetPositionAndRotation(m_camera.position, m_camera.rotation);
+    }
+}
---
+++ unity-arkit-plugin/Assets/sada/PhotonShare/Script/PhotonConnect.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhotonConnect : MonoBehaviour
+{
+    [SerializeField] GameObject CameraObj;
+ 
+    void Start()
+    {
+        // Photonに接続する(引数でゲームのバージョンを指定できる)
+        PhotonNetwork.ConnectUsingSettings(null);
+
+        //今回オートでロビーにJoinする
+    }
+
+    // ロビーに入ると呼ばれる
+    void OnJoinedLobby()
+    {
+        Debug.Log("ロビーに入りました。");
+
+        // ルームに入室する
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    // ルームに入室すると呼ばれる
+    void OnJoinedRoom()
+    {
+        Debug.Log("ルームへ入室しました。");
+        PhotonNetwork.Instantiate(CameraObj.name,transform.position,transform.rotation,0);
+        //Resourcesの中にある生成プレハブの名前
+
+    }
+
+    // ルームの入室に失敗すると呼ばれる
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("ルームの入室に失敗しました。");
+
+        // ルームがないと入室に失敗するため、その時は自分で作る
+        // 引数でルーム名を指定できる
+        PhotonNetwork.CreateRoom("ARKitShare");
+    }
+}
